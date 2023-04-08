cbe8ec2d4aa06ed573e0b11f8582ce74f7796289
elGATO <122099031+elGATOCoin@users.noreply.github.com>
2023-04-08 05:33:26 +0100

    Add files via upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x737eF178B04DEf01D02FaDEeb9a666d2628B9E14";
+export const tokenContractAddress =
+  "0x634f86B9BBa1ff052f72BF3fdaB1823d0BA86DDC";
+export const stakingContractAddress =
+  "0x0A12675c16440D534DCDa8C3aA6F0ed7e6AA0a80";
